6f999ba5ca3c35f3f09305a3bc497813542513ad
Jérémy Caldas <jeremy@fifty-five.com>
2017-12-12 10:56:26 +0100

    zsh : Now use nvim

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,4 +2,4 @@ alias reload!='. ~/.zshrc'
 alias colors256='for code in {0..255}; do echo -e "\e[38;05;${code}m $code: Test"; done'
 alias rg='ranger'
 alias op='rifle'
-alias tea="notify-send -i $DOTFILES/resources/tea.png 'Tea time' 'Infusing...'; sleep 180 ; notify-send -t 600000 -i $DOTFILES/resources/tea.png 'Tea time' 'Your tea is ready\!\rPlease remove the leaves and enjoy your tea.'"
+alias vi=nvim
